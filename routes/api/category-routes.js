219f5b10ba5a35393c86f6c7fd8e2a155960c98d
chasewaldron2400 <chasew2400@gmail.com>
2024-11-03 23:29:22 -0600

    started routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,17 +1,32 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findAll } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }] 
+      category_name: req.body.category_name,
+    });
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 router.post('/', (req, res) => {
   // create a new category
